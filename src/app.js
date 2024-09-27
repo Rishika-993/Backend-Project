ef0b59a2c7f107426af2387b581964fab96aac3f
Rishika-993 <rishikaagrawal22@gmail.com>
2024-09-27 23:10:14 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit: '16kb'}));
+
 export {app};
