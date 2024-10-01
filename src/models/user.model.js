00b1bd34eb2ef1b1b4684c4bc78ddaa7469d7c0b
Rishika-993 <rishikaagrawal22@gmail.com>
2024-10-01 23:30:32 +0530

    jwt token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,4 +65,11 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
+userSchema.methods.generateToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+}
+
 export const User = mongoose.model("User", userSchema);
