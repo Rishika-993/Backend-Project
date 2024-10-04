1ccf8ec891858a9aa09979f8fddf519425d34149
Rishika-993 <rishikaagrawal22@gmail.com>
2024-10-04 23:28:19 +0530

    clodinary file storage

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -7,3 +7,34 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        //upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto",
+        })
+        //file has been uploaded succesfully
+        console.log("File uploaded successfully", response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath)     //delete the file from local storage
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+// // Upload an image
+// const uploadResult = await cloudinary.uploader
+// .upload(
+//     'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//         public_id: 'shoes',
+//     }
+// )
+// .catch((error) => {
+//     console.log(error);
+// });
+
+// console.log(uploadResult);
